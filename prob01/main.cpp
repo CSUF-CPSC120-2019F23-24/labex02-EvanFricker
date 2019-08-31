b3a632c088418f9a5b88f3ee925e9d1ce5d977a2
frickere <frickere@cs104u-5a.acad.fullerton.edu>
2019-08-31 13:30:21 -0700

    Displays a diamond

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,16 @@
-// Name: 
+// Name: Diamond
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
 
 int main()
 {
-
+  std::cout << "    *    " << '\n';   // Lines 8-14 will display a diamond when function is activated
+  std::cout << "   ***    " << '\n';
+  std::cout << "  *****    " << '\n';
+  std::cout << " *******   " << '\n';
+  std::cout << "  *****    " << '\n';
+  std::cout << "   ***    " << '\n';
+  std::cout << "    *    " << '\n';
   return 0;
 }
